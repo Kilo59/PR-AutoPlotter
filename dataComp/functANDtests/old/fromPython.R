292a718ec269009a7b3a7b46f6dc1ccf83cf7d86
Gabe <goregk@guilford.edu>
2016-07-29 16:15:31 -0400

    Delete Unused

--- dataComp/functANDtests/old/fromPython.R
+++
@@ -1,9 +0,0 @@
-# fromPython.R
-# Fetch command line arguments
-myArgs <- commandArgs(trailingOnly = TRUE)
-
-# Convert to numerics
-nums = as.numeric(myArgs)
-
-# cat will write the result to the stdout stream
-cat(max(nums))
